601102d37716ec51cacd816d4f3c291c1aee7521
VENI SAISUMANI <VENI SAISUMANI@LAPTOP-1UT8GC0E>
2020-04-23 10:58:46 +0530

    update

---
+++ otms-update-and-delete-test/src/main/java/com/capg/otms/updateanddeletetest/OtmsUpdateAndDeleteTestApplication.java
@@ -0,0 +1,13 @@
+package com.capg.otms.updateanddeletetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtmsUpdateAndDeleteTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtmsUpdateAndDeleteTestApplication.class, args);
+	}
+
+}
---
+++ otms-update-and-delete-test/src/test/java/com/capg/otms/updateanddeletetest/OtmsUpdateAndDeleteTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capg.otms.updateanddeletetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtmsUpdateAndDeleteTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
