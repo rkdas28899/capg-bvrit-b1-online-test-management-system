1a9b8f51399eec04198fdb6e2289c38ccccb0aa2
gorthi <gorthi@DESKTOP-6OETV4J>
2020-04-23 20:38:26 +0530

    added entities

---
+++ otms-user/src/main/java/com/capg/otms/user/OtmsUserApplication.java
@@ -0,0 +1,13 @@
+package com.capg.otms.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtmsUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtmsUserApplication.class, args);
+	}
+
+}
---
+++ otms-user/src/test/java/com/capg/otms/user/OtmsUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capg.otms.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtmsUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
