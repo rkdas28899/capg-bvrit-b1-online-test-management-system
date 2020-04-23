34136b84dfafb9c7452449dab6d165dddbc99bc2
admin <admin@DESKTOP-5U63BG0.Dlink>
2020-04-23 11:31:22 +0530

    Added Model Classes

---
+++ otms-add-test/src/main/java/com/capg/otms/addandassigntest/model/Question.java
@@ -0,0 +1,57 @@
+package com.capg.otms.addandassigntest.model;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.ArrayList;
+
+public class Question{
+	private BigInteger questionId;
+	private ArrayList<String> questionOptions;
+	private String questionTitle;
+	private Integer questionAnswer;
+	private BigDecimal questionMarks;
+	private Integer chosenAnswer;
+	private BigDecimal marksScored;
+	public BigInteger getQuestionId() {
+		return questionId;
+	}
+	public void setQuestionId(BigInteger questionId) {
+		this.questionId = questionId;
+	}
+	public ArrayList<String> getQuestionOptions() {
+		return questionOptions;
+	}
+	public void setQuestionOptions(ArrayList<String> questionOptions) {
+		this.questionOptions = questionOptions;
+	}
+	public String getQuestionTitle() {
+		return questionTitle;
+	}
+	public void setQuestionTitle(String questionTitle) {
+		this.questionTitle = questionTitle;
+	}
+	public Integer getQuestionAnswer() {
+		return questionAnswer;
+	}
+	public void setQuestionAnswer(Integer questionAnswer) {
+		this.questionAnswer = questionAnswer;
+	}
+	public BigDecimal getQuestionMarks() {
+		return questionMarks;
+	}
+	public void setQuestionMarks(BigDecimal questionMarks) {
+		this.questionMarks = questionMarks;
+	}
+	public Integer getChosenAnswer() {
+		return chosenAnswer;
+	}
+	public void setChosenAnswer(Integer chosenAnswer) {
+		this.chosenAnswer = chosenAnswer;
+	}
+	public BigDecimal getMarksScored() {
+		return marksScored;
+	}
+	public void setMarksScored(BigDecimal marksScored) {
+		this.marksScored = marksScored;
+	}
+}
---
+++ otms-add-test/src/main/java/com/capg/otms/addandassigntest/model/User.java
@@ -0,0 +1,51 @@
+package com.capg.otms.addandassigntest.model;
+
+public class User {
+	private Long userId; 
+	private String userName;
+	private Test userTest;
+	private Boolean isAdmin; 
+	private String userPassword;
+	public User(String userName, Boolean isAdmin, String userPassword) {
+		super();
+		this.userName = userName;
+		this.isAdmin = isAdmin;
+		this.userPassword = userPassword;
+	}
+	public User(String userName, Test userTest) {
+		super();
+		this.userName = userName;
+		this.userTest = userTest;
+	}
+
+	public Long getUserId() {
+		return userId;
+	}
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public Test getUserTest() {
+		return userTest;
+	}
+	public void setUserTest(Test userTest) {
+		this.userTest = userTest;
+	}
+	public Boolean getIsAdmin() {
+		return isAdmin;
+	}
+	public void setIsAdmin(Boolean isAdmin) {
+		this.isAdmin = isAdmin;
+	}
+	public String getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	} 
+}
